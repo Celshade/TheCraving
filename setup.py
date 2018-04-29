06b92338276f3776ec6da2e53dd246c6c0698c64
Celshade <ggCelshade@gmail.com>
2018-04-29 13:37:19 -0400

    Update version and dev status in setup.py

--- setup.py
+++ setup.py
@@ -13,21 +13,22 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='TheCraving',
-    version='1.0.0',
+    version='1.2.0b1',
     description='A simple text-based adventure game',
     long_description=long_description,
     long_description_content_type='text/markdown',
     url='https://github.com/Celshade/The-Craving',
-    license="MIT License",
-    author='D.Collins aka Celshade',
+    author='Danny Collins aka Celshade',
     author_email='ggcelshade@gmail.com',
+    license="MIT License",
     classifiers=[
-        'Development Status :: 3 - Alpha',
+        'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
         'Topic :: Game Development',
         'License :: OSI approved :: MIT License',
         'Programming Language :: Python :: 3.6'
         ],
+    python_requires='>=3',
     keywords='game textadventure thecraving',
     packages=find_packages(),
     project_urls={
