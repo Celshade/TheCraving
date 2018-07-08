bfc8d380bccda67afda3022b34616ccb45f75838
Celshade <ggCelshade@gmail.com>
2018-07-08 00:23:43 -0400

    Update version (patch).

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='TheCraving',
-    version='0.2.0b1',
+    version='0.2.0b2',
     description='A simple text-based adventure game',
     long_description=long_description,
     long_description_content_type='text/markdown',
