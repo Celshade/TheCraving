c04a590e799e5bed97ff06a61be0a1b596f7e69e
Celshade <ggCelshade@gmail.com>
2018-08-17 11:37:13 -0400

    Update MiniMap.render() to eval # of ROOMS.

--- thecraving/cmap.py
+++ thecraving/cmap.py
@@ -41,49 +41,54 @@ class MiniMap(object):
         render()
         run()
     """
+
     def __init__(self, tilesize: int, width: int, height: int) -> None:
-        """The start() method will be called, upon instantiation, to speed up
-        map rendering.
-        """
+        """Initialize necessary pygame modules to speed up map rendering."""
         self.TSIZE = tilesize
         self.WIDTH = width
         self.HEIGHT = height
         self.MWIDTH = self.TSIZE * self.WIDTH
         self.MHEIGHT = self.TSIZE * self.HEIGHT
-        self.start()
 
-    def start(self) -> None:
-        """Initiate necessary pygame modules."""
-        # call individual modules for speed boost
         pygame.__init__("display")
         pygame.__init__("event")
         pygame.__init__("draw")
         pygame.font.init()
 
-    def render(self) -> None:
-        """Render the map."""
-        rect = pygame.draw.rect
-        DSPLAY = pygame.display.set_mode((self.MWIDTH + 10, self.MHEIGHT + 40))
+    def render(self, num: int=1) -> None:
+        """Render the map.
+
+        Args:
+            num: The number of rooms to render (default=1).
+        """
+        # Ignore flake8 error
+        RECT = pygame.draw.rect
+        # Store RECT data as a string for later eval.
+        ROOMS = [
+            "RECT(DSPLY, WHITE, (self.TSIZE * 3, self.TSIZE * 6, 60, 60), 1)",
+            "RECT(DSPLY, WHITE, (self.TSIZE * 3, self.TSIZE * 6, 60, 60), 1)",
+            "RECT(DSPLY, BLUE, (self.TSIZE * 3, self.TSIZE * 4, 60, 60), 1)",
+            "RECT(DSPLY, PURPLE, (self.TSIZE, self.TSIZE * 4, 60, 60), 1)",
+            "RECT(DSPLY, GREEN, (self.TSIZE * 5, self.TSIZE * 4, 60, 60), 1)",
+            "RECT(DSPLY, RED, (self.TSIZE, self.TSIZE * 2, 60, 60), 1)",
+            "RECT(DSPLY, YELLOW, (self.TSIZE * 5, self.TSIZE * 2, 60, 60), 1)",
+            "RECT(DSPLY, ORANGE, (self.TSIZE * 3, self.TSIZE * 2, 60, 60), 1)",
+            "RECT(DSPLY, GRAY, (self.TSIZE * 3, 10, 60, 60), 1)"
+        ]
+        DSPLY = pygame.display.set_mode((self.MWIDTH + 10, self.MHEIGHT + 40))
         FONT = pygame.font.Font(None, 18)
         text = FONT.render("Press [Q] to return.", True, WHITE, BLACK)
 
         pygame.display.set_caption("The C R A V I N G")
-        # background
-        DSPLAY.fill(BLACK)
-        # ROOMS
-        rect(DSPLAY, WHITE, (self.TSIZE * 3, self.TSIZE * 6, 60, 60), 1)
-        rect(DSPLAY, BLUE, (self.TSIZE * 3, self.TSIZE * 4, 60, 60), 1)
-        rect(DSPLAY, PURPLE, (self.TSIZE, self.TSIZE * 4, 60, 60), 1)
-        rect(DSPLAY, GREEN, (self.TSIZE * 5, self.TSIZE * 4, 60, 60), 1)
-        rect(DSPLAY, RED, (self.TSIZE, self.TSIZE * 2, 60, 60), 1)
-        rect(DSPLAY, YELLOW, (self.TSIZE * 5, self.TSIZE * 2, 60, 60), 1)
-        rect(DSPLAY, ORANGE, (self.TSIZE * 3, self.TSIZE * 2, 60, 60), 1)
-        rect(DSPLAY, GRAY, (self.TSIZE * 3, 10, 60, 60), 1)
-        DSPLAY.blit(text, (self.TSIZE * 3 - 25, self.MHEIGHT + 20))
-        # TODO coordinate with Player.go() to show current/discovered room(s).
+        # Render background.
+        DSPLY.fill(BLACK)
+        # Render the necessary ROOMS.
+        for x in range(0, num + 1):
+            eval(ROOMS[x])
+        DSPLY.blit(text, (self.TSIZE * 3 - 25, self.MHEIGHT + 20))
 
     def run(self) -> None:
-        """Call self.render(), update DSPLAY, and close pygame when done."""
+        """Call self.render(), update DSPLY, and close pygame when done."""
         while True:
             self.render()
             for event in pygame.event.get():
@@ -98,6 +103,6 @@ class MiniMap(object):
 
 
 # TODO clean up hard-code
-# # For testing purposes
-# test = MiniMap(60, 7, 7)
-# test.run()
+# For testing purposes
+test = MiniMap(60, 7, 7)
+test.run()
